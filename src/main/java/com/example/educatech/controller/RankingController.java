3b14baee99a76e0a1425ac10303c906ec714e6e6
MuriloSobral17 <murilosobrallisboadasilva12@gmail.com>
2025-11-17 10:53:36 -0300

    adicionando ranking

---
+++ src/main/java/com/example/educatech/controller/RankingController.java
@@ -0,0 +1,5 @@
+package com.example.educatech.controller;
+
+public class RankingController {
+
+}
---
+++ src/main/java/com/example/educatech/repository/RankingRepository.java
@@ -0,0 +1,5 @@
+package com.example.educatech.repository;
+
+public class RankingRepository {
+
+}
